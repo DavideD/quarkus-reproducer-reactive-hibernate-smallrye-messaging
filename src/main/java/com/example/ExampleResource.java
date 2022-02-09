5dd0807e770fc9144a5ef07fc76880a935c00f90
Davide <davide@hibernate.org>
2022-02-09 19:18:03 +0000

    Refactoring

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -33,8 +33,8 @@ public class ExampleResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Uni<Response> invokeFetchEntityAndGreetAndLog(@QueryParam("id") Long id) {
         return findItemById(id)
-                .onItem().invoke(entity -> eventEmitter.send(event(entity)))
-                .onItem().transform(entity -> Response.accepted(entity).build());
+                .call(entity -> Uni.createFrom().completionStage(eventEmitter.send(event(entity))))
+                .map(entity -> Response.accepted(entity).build());
     }
 
     @GET
@@ -42,8 +42,8 @@ public class ExampleResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Uni<Response> callFetchEntityAndGreetAndLog(@QueryParam("id") Long id) {
         return findItemById(id)
-                .onItem().call(entity -> Uni.createFrom().completionStage(eventEmitter.send(event(entity))))
-                .onItem().transform(entity -> Response.accepted(entity).build());
+                .call(entity -> Uni.createFrom().completionStage(eventEmitter.send(event(entity))))
+                .map(entity -> Response.accepted(entity).build());
     }
 
     private Uni<MyEntity> findItemById(Long id) {
--- src/main/java/com/example/MyService.java
+++ src/main/java/com/example/MyService.java
@@ -1,10 +1,11 @@
 package com.example;
 
-import io.quarkus.hibernate.reactive.panache.Panache;
+import javax.enterprise.context.ApplicationScoped;
+
+import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
+import io.smallrye.mutiny.Uni;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
-
-import javax.enterprise.context.ApplicationScoped;
 
 /**
  * Created by Marian at 31.01.2022
@@ -14,19 +15,14 @@ import javax.enterprise.context.ApplicationScoped;
 public class MyService {
 
     @Incoming(Event.FIRE_AND_FORGET)
-    public void fireForget(Event event) {
-        Panache.withTransaction(event::persist)
-                .onFailure().invoke(throwable -> log.error("Failed to store event", throwable))
-                .subscribe().with(panacheEntityBase -> {
-                });
+    @ReactiveTransactional
+    public Uni<Event> fireForget(Event event) {
+        return event.persist();
     }
 
     @Incoming("event-uni")
-    public void onMessage(Event event) {
-        Panache.withTransaction(event::persist)
-                .onFailure().invoke(throwable -> log.error("Failed to store event", throwable))
-                .subscribe().with(panacheEntityBase -> {
-                });
+    @ReactiveTransactional
+    public Uni<Event> onMessage(Event event) {
+        return event.persist();
     }
-
 }

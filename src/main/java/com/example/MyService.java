3122d7d20c24568e921e1231401aec9220f20684
mklueh90 <marianklueh@gmail.com>
2022-02-09 18:14:11 +0100

    testing with @ReactiveTransactional

--- src/main/java/com/example/MyService.java
+++ src/main/java/com/example/MyService.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import io.quarkus.hibernate.reactive.panache.Panache;
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
+import io.smallrye.mutiny.Uni;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
@@ -13,12 +16,11 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class MyService {
 
+
+    @ReactiveTransactional
     @Incoming(Event.FIRE_AND_FORGET)
-    public void fireForget(Event event) {
-        Panache.withTransaction(event::persist)
-                .onFailure().invoke(throwable -> log.error("Failed to store event", throwable))
-                .subscribe().with(panacheEntityBase -> {
-                });
+    public Uni<PanacheEntityBase> fireForget(Event event) {
+        return event.persist().onItem().invoke(panacheEntityBase -> log.info("persist"));
     }
 
     @Incoming("event-uni")
